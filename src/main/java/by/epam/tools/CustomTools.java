93e9fa4226f52faa0dbf4fb088c36e8588a0542f
VictorLay <Viktor.latyshey@mail.ru>
2022-03-29 16:59:11 +0300

    Created custom split method for solve custom Task with string split

---
+++ src/main/java/by/epam/tools/CustomTools.java
@@ -0,0 +1,50 @@
+package by.epam.tools;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CustomTools {
+
+  public String[] customSplit(String text, String regex) {
+
+    Logger logger = LogManager.getLogger(CustomTools.class);
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    int indexOfStringStart = 0;
+    ArrayList<String> arrayOfSplitString = new ArrayList<>();
+    while (matcher.find()) {
+      arrayOfSplitString.add(text.substring(indexOfStringStart, matcher.end()));
+      indexOfStringStart = matcher.end();
+    }
+
+
+    if (indexOfStringStart != text.length()) {
+      arrayOfSplitString.add(text.substring(indexOfStringStart));
+    }
+    arrayOfSplitString.remove(regex);
+    String response[]= new String[arrayOfSplitString.size()];
+    for (int i = 0; i < arrayOfSplitString.size(); i++) {
+      response[i] = arrayOfSplitString.get(i).trim();
+    }
+
+    return response;
+
+
+//    String response[];
+//    if (indexOfStringStart != text.length()) {
+//      response = new String[arrayOfSplitString.size() + 1];
+//      response[arrayOfSplitString.size()] = text.substring(indexOfStringStart);
+//    } else {
+//      response = new String[arrayOfSplitString.size()];
+//    }
+//
+//    for (int i = 0; i < arrayOfSplitString.size(); i++) {
+//      response[i] = arrayOfSplitString.get(i);
+//    }
+//
+//    return response;
+  }
+}
