8764e0b6f83cfde5f78b2f23f7077a1f91092360
VictorLay <Viktor.latyshey@mail.ru>
2022-03-30 23:47:15 +0300

    Created abstract class for composites.

---
+++ src/main/java/by/epam/composite/entities/BaseTextStructure.java
@@ -0,0 +1,10 @@
+package by.epam.composite.entities;
+
+public abstract class BaseTextStructure {
+
+  abstract public void setInstance(String data);
+
+  abstract public String getInstance();
+
+
+}
