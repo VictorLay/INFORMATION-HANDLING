d63ae59aef0b41ecd2eceb97399c971239edb5e4
VictorLay <Viktor.latyshey@mail.ru>
2022-04-01 23:42:27 +0300

    Class got field of fatherNode for convenient work with tree model of composite.

--- src/main/java/by/epam/composite/entities/BaseTextStructure.java
+++ src/main/java/by/epam/composite/entities/BaseTextStructure.java
@@ -5,10 +5,33 @@ import java.util.List;
 
 public abstract class BaseTextStructure {
 
+  private BaseTextStructure fatherNode;
   private List<BaseTextStructure> childNodes;
+  private Object nodeObject;
 
-  public BaseTextStructure() {
+  public BaseTextStructure getFatherNode() {
+    return fatherNode;
+  }
+
+  public void setFatherNode(BaseTextStructure fatherNode) {
+    this.fatherNode = fatherNode;
+  }
+
+  public Object getNodeObject() {
+    return nodeObject;
+  }
+
+  public void setNodeObject(Object nodeObject) {
+    this.nodeObject = nodeObject;
+  }
+
+
+  protected BaseTextStructure() {
     childNodes = new ArrayList<>();
+  }
+
+  protected BaseTextStructure(List<BaseTextStructure> childNodes) {
+    this.childNodes = childNodes;
   }
 
   public List<BaseTextStructure> getChildNodes() {
@@ -18,5 +41,4 @@ public abstract class BaseTextStructure {
   public void setChildNodes(List<BaseTextStructure> childNodes) {
     this.childNodes = childNodes;
   }
-//todo replace the initialization at father class BaseStructure
 }
