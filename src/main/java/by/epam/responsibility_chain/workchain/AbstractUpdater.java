4f55c20d9dba687d512344ce145ea2b0204e9acf
VictorLay <Viktor.latyshey@mail.ru>
2022-04-02 18:51:44 +0300

    Abstract class for chain pattern with {BaseTextStructure nextDoSomething(BaseTextStructure dataTxt)} execute method configuration

---
+++ src/main/java/by/epam/responsibility_chain/workchain/AbstractUpdater.java
@@ -0,0 +1,27 @@
+package by.epam.responsibility_chain.workchain;
+
+import by.epam.composite.entities.BaseTextStructure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class AbstractUpdater {
+
+  private AbstractUpdater next;
+
+  public AbstractUpdater bindNextLink(AbstractUpdater next) {
+    this.next = next;
+    return next;
+  }
+
+  public abstract BaseTextStructure doSomething(BaseTextStructure dataTxt);
+
+  public BaseTextStructure nextDoSomething(BaseTextStructure dataTxt) {
+    if (next == null) {
+      Logger log = LogManager.getLogger();
+      log.error("The error is possible. The method 'nextDoSomething' return the null value");
+      return null;
+      // todo replace null to another value
+    }
+    return next.doSomething(dataTxt);
+  }
+}
