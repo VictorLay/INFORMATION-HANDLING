dd03dcf644bab09be3ca9c6aa2d43cd5f507cc13
VictorLay <Viktor.latyshey@mail.ru>
2022-04-05 20:17:21 +0300

    chain server refactoring. Added remove method for cleaning installed classes chain.

--- src/main/java/by/epam/responsibility_chain/Server.java
+++ src/main/java/by/epam/responsibility_chain/Server.java
@@ -2,14 +2,14 @@ package by.epam.responsibility_chain;
 
 import by.epam.composite.entities.BaseTextStructure;
 import by.epam.responsibility_chain.create.*;
-import by.epam.responsibility_chain.strchain.AbstractReader;
-import by.epam.responsibility_chain.workchain.AbstractWorker;
+import by.epam.responsibility_chain.read.AbstractReader;
+import by.epam.responsibility_chain.update.AbstractUpdater;
 
 
 public class Server {
 
   private AbstractCreator abstractCreator;
-  private AbstractWorker abstractWorker;
+  private AbstractUpdater abstractUpdater;
   private AbstractReader abstractReader;
 
   public AbstractCreator setAbstractCreator(AbstractCreator abstractCreator) {
@@ -17,26 +17,32 @@ public class Server {
     return this.abstractCreator;
   }
 
-  public AbstractReader setAbstractReceiver(AbstractReader abstractReader) {
+  public AbstractReader setAbstractReader(AbstractReader abstractReader) {
     this.abstractReader = abstractReader;
     return this.abstractReader;
   }
 
-  public AbstractWorker setAbstractWorker(AbstractWorker abstractWorker) {
-    this.abstractWorker = abstractWorker;
-    return this.abstractWorker;
+  public AbstractUpdater setAbstractWorker(AbstractUpdater abstractUpdater) {
+    this.abstractUpdater = abstractUpdater;
+    return this.abstractUpdater;
   }
 
   public BaseTextStructure useCreatorChain(String textData){
     return abstractCreator.doSomething(textData);
   }
 
-  public BaseTextStructure chainAccessTestMethod(BaseTextStructure structure){
-    return abstractWorker.doSomething(structure);
+  public BaseTextStructure useAbstractUpdater(BaseTextStructure structure){
+    return abstractUpdater.doSomething(structure);
   }
 
   public String useReaderChain(BaseTextStructure textStructure){
     return abstractReader.doSomething(textStructure);
+  }
+
+  public void removeAll(){
+    abstractReader = null;
+    abstractCreator = null;
+    abstractUpdater = null;
   }
 
 }
