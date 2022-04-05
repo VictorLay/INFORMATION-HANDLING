d16b7e95f757b870a336794ad9640bdcb2738b7d
VictorLay <Viktor.latyshey@mail.ru>
2022-04-05 10:47:18 +0300

    Created bitwise OR operation in the command class

---
+++ src/main/java/by/epam/interpreter/controller/command/impl/BitwiseOr.java
@@ -0,0 +1,15 @@
+package by.epam.interpreter.controller.command.impl;
+
+import by.epam.interpreter.Context;
+import by.epam.interpreter.Expression;
+import by.epam.interpreter.controller.command.Command;
+
+public class BitwiseOr implements Command {
+
+  @Override
+  public String calculate(Context context) {
+    Expression leftShiftExpr = (cont) -> String.valueOf(
+        Integer.parseInt(cont.getLeftOperand()) | Integer.parseInt(cont.getRightOperand()));
+    return leftShiftExpr.interpret(context);
+  }
+}
