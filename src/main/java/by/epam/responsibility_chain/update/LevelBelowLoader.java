0eedee6c19db658ac588bc36a382c7d454dc7709
VictorLay <Viktor.latyshey@mail.ru>
2022-04-06 14:39:44 +0300

    replaced returning null to throwing exception

--- src/main/java/by/epam/responsibility_chain/update/LevelBelowLoader.java
+++ src/main/java/by/epam/responsibility_chain/update/LevelBelowLoader.java
@@ -1,6 +1,8 @@
 package by.epam.responsibility_chain.update;
 
 import by.epam.composite.entities.BaseTextStructure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LevelBelowLoader extends AbstractUpdater{
 
@@ -8,7 +10,13 @@ public class LevelBelowLoader extends AbstractUpdater{
   public BaseTextStructure doSomething(BaseTextStructure dataTxt) {
 
     for (BaseTextStructure chileNode : dataTxt.getChildNodes()){
-      nextDoSomething(chileNode);
+      try {
+        nextDoSomething(chileNode);
+      } catch (Exception e) {
+        Logger log = LogManager.getLogger();
+        log.error("Achieve the last chain element");
+        return dataTxt;
+      }
     }
     return dataTxt;
   }
