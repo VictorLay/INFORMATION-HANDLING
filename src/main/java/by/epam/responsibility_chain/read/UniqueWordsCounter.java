83059305b895cbaa19320d78247e1ce5f6159ff8
VictorLay <Viktor.latyshey@mail.ru>
2022-04-06 12:39:32 +0300

    decomposition class operations to private methods

--- src/main/java/by/epam/responsibility_chain/read/UniqueWordsCounter.java
+++ src/main/java/by/epam/responsibility_chain/read/UniqueWordsCounter.java
@@ -1,29 +1,45 @@
 package by.epam.responsibility_chain.read;
 
 import by.epam.composite.entities.BaseTextStructure;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
 
 public class UniqueWordsCounter extends AbstractReader {
 
   @Override
   public String doSomething(BaseTextStructure data) {
-    List<String> words = data.getChildNodes().stream().flatMap(p -> p.getChildNodes().stream())
-        .flatMap(s -> s.getChildNodes().stream()).map(
-            w -> w.toString().replaceAll("([.,?!]\\B)|(\\(|\\))|(\\A-\\Z)|“|”", "")
-                .toLowerCase(Locale.ROOT)).filter(x -> x.matches("[^.:?!=]+"))
-        .collect(Collectors.toList());
+    List<String> words = separateSingleWords(data);
+    StringBuilder countedWordsStringResponse = new StringBuilder();
 
-    StringBuilder response = new StringBuilder();
+    for (String countedWord : countUniqueWordsInList(words)){
+      countedWordsStringResponse.append(countedWord);
+    }
+
+    return countedWordsStringResponse.toString();
+  }
+
+  @NotNull
+  private List<String> countUniqueWordsInList(List<String> words) {
+    List<String> countedWordsList = new ArrayList<>();
     for (int i = 0; i < words.size(); ) {
       String word = words.get(i);
-      response.append("{" + word + ":" + words.stream().filter(x -> x.equals(word)).count()
+      countedWordsList.add("{" + word + ":" + words.stream().filter(x -> x.equals(word)).count()
           + "}\s");
       words = words.stream().filter(x -> !(x.equals(word))).collect(Collectors.toList());
       i = 0;
     }
+    return countedWordsList;
+  }
 
-    return response.toString();
+  @NotNull
+  private List<String> separateSingleWords(BaseTextStructure data) {
+    return data.getChildNodes().stream().flatMap(p -> p.getChildNodes().stream())
+        .flatMap(s -> s.getChildNodes().stream()).map(
+            w -> w.toString().replaceAll("([.,?!]\\B)|(\\(|\\))|(\\A-\\Z)|“|”", "")
+                .toLowerCase(Locale.ROOT)).filter(x -> x.matches("[^.:?!=]+"))
+        .collect(Collectors.toList());
   }
 }
