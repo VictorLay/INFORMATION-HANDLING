ac906746e382cc605180964e7ddd0591b5e1a4ca
VictorLay <Viktor.latyshey@mail.ru>
2022-04-06 14:46:25 +0300

    added quantity of operands validation.

--- src/main/java/by/epam/interpreter/controller/Controller.java
+++ src/main/java/by/epam/interpreter/controller/Controller.java
@@ -3,17 +3,20 @@ package by.epam.interpreter.controller;
 
 import by.epam.interpreter.Context;
 import by.epam.interpreter.controller.command.Command;
-import by.epam.interpreter.controller.command.Operators;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class Controller {
 
+  private static final int ONE_OPERAND = 1;
+  private static final int TWO_OPERANDS = 2;
   private final CommandProvider provider = new CommandProvider();
 
   public String executeSimpleExpression(String simpleBinaryExpression) {
 
-//    Pattern pattern = Pattern.compile("\\D+");
     Pattern pattern = Pattern.compile("[^-+0-9]+");
     Matcher operatorFinder = pattern.matcher(simpleBinaryExpression);
     operatorFinder.find();
@@ -21,8 +24,21 @@ public final class Controller {
         operatorFinder.end());
 
     Command operationAction = provider.getOperationActionByOperator(operator);
-    Context context = new Context(simpleBinaryExpression.split(operator)[0],
-        simpleBinaryExpression.split(operator)[1]);
+    Context context;
+    switch (StringUtils.split(simpleBinaryExpression, operator).length) {
+      case TWO_OPERANDS:
+        context = new Context(StringUtils.split(simpleBinaryExpression, operator)[0],
+            StringUtils.split(simpleBinaryExpression, operator)[1]);
+        break;
+      case ONE_OPERAND:
+        context = new Context("",
+            StringUtils.split(simpleBinaryExpression, operator)[0]);
+        break;
+      default:
+        context = new Context("", "");
+        Logger log = LogManager.getLogger();
+        log.error("Quantity of operands in bitwise expression is illegal value.");
+    }
 
     return operationAction.calculate(context);
   }
