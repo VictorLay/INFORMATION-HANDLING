f6e93222756806dc77ca0f9057c18af8d9718530
VictorLay <Viktor.latyshey@mail.ru>
2022-04-06 13:36:52 +0300

    Created chain classes for counting vowel and consonant letters.

---
+++ src/main/java/by/epam/responsibility_chain/read/ConsonantLettersCounter.java
@@ -0,0 +1,14 @@
+package by.epam.responsibility_chain.read;
+
+import by.epam.composite.entities.BaseTextStructure;
+import java.util.Locale;
+
+public class ConsonantLettersCounter extends AbstractReader{
+
+  @Override
+  public String doSomething(BaseTextStructure data) {
+    return String.valueOf(data.getChildNodes().stream().flatMap(p -> p.getChildNodes().stream())
+        .flatMap(s -> s.getChildNodes().stream()).flatMap(w -> w.getChildNodes().stream())
+        .filter(x -> x.toString().toUpperCase(Locale.ROOT).matches("[BCDFGHJKLMNPQRSTVWXZ]")).count());
+  }
+}
